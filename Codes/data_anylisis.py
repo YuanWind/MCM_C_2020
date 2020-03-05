cf7d55b0f35fe4f6c8a561becd0375ebd92967d9
wind <yywind@126.com>
2020-03-06 07:07:15 +0800

    init

---
+++ Codes/data_anylisis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+hair_dryer=pd.read_csv('Data/hair_dryer.tsv',sep='\t',encoding='utf-8')
+microwave=pd.read_csv('Data/microwave.tsv',sep='\t',encoding='utf-8')
+pacifier=pd.read_csv('Data/pacifier.tsv',sep='\t',encoding='utf-8')
+print()
